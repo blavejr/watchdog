998149f50d38ce6393ac9ffaac2772ee6ea46652
Naven Prasad <navenprasad25@gmail.com>
2016-11-11 10:15:42 +0800

    list views

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello World!")
+    all_posts = Post.objects.all()
+    context = {'all_posts': all_posts}
+    return(request, 'index.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post , pk=post_id)
+    return render(request, 'detail.html', {'post': post})
