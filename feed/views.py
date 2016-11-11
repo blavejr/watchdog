98f218a4487db6c8ad15f98145c392274f25c7cf
Naven Prasad <navenprasad25@gmail.com>
2016-11-11 11:19:27 +0800

    views complete

--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post
 
+
 def index(request):
-    all_posts = Post.objects.all()
-    context = {'all_posts': all_posts}
-    return(request, 'index.html', context)
+    latest_post_list = Post.objects.order_by('-created')[:10]
+    context = {'latest_post_list': latest_post_list}
+    return render(request, 'index.html', context)
 
 def detail(request, post_id):
-    post = get_object_or_404(Post , pk=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     return render(request, 'detail.html', {'post': post})
