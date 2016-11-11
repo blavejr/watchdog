57cc35debc1e2fd07c19dfe6b6edf5b0fca4f6a0
Naven Prasad <navenprasad25@gmail.com>
2016-11-11 12:57:36 +0800

    Forms working

--- feed/views.py
+++ feed/views.py
@@ -7,11 +7,32 @@ from .models import Post
 def index(request):
     latest_post_list = Post.objects.order_by('-created')[:10]
     form = PostForm()
-    context = {'latest_post_list': latest_post_list, 'form' : form}
+    user = request.user
+    context = {'latest_post_list': latest_post_list, 'form' : form, 'user': user}
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+    else:
+        form = PostForm()
+    
     return render(request, 'index.html', context)
     
-
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+    else:
+        form = PostForm()
+    return render(request, 'edit.html', {'form': form})
