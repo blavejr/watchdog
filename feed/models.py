887a201dbc1d902679f87e20ab9ad06e4761a404
Naven Prasad <navenprasad25@gmail.com>
2016-11-11 09:19:08 +0800

    Posts done

--- feed/models.py
+++ feed/models.py
@@ -1,5 +1,13 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(User, related_name='feed_post')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. Welcome to WatchDog")
+    return HttpResponse("Hello World!")
